db5f661ddb0f1665307c9b59782e56b677cae5f8
JoshuaWalk <joshuassignup@gmail.com>
2021-06-25 13:43:41 -0600

    removed params.py

--- tests/params.py
+++
@@ -1,9 +0,0 @@
-
-low_cards = [
-    ('Spades', 3, '3 of Spades'),
-    ('Diamonds', 4, '4 of Diamonds'),
-    ('Diamonds', 7, '7 of Diamonds'),
-    ('Hearts', 2, '2 of Hearts'),
-    ('Spades', 'Ace', 'Ace of Spades'),
-    ('Clubs', 3, '3 of Clubs')
-]
